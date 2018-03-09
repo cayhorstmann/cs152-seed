2886e82b31d1af8bce89c4a03b30670f96c9453c
Cay Horstmann <cay@horstmann.com>
2018-03-08 20:04:31 -0800

    ...

--- src/main/g8/src/main/scala/Hello.scala
+++ src/main/g8/src/main/scala/Hello.scala
@@ -1,8 +1,16 @@
-object Hello extends App {
-  val greeter = new Greeter("CS152 students")
-  println(greeter.greet)
-}
+import scala.util.parsing.combinator._
 
 class Greeter(subject: String) {
   def greet = "Hello, " + subject + "!"
+}
+
+class GreeterParser extends JavaTokenParsers {
+  def statement: Parser[Greeter] = "greet" ~> stringLiteral ^^ 
+    (s => new Greeter(s))
+}
+
+object Hello extends App {
+  val parser = new GreeterParser
+  val result = parser.parseAll(parser.statement, """greet "CS152 students" """)
+  println(result.get.greet)     
 }
