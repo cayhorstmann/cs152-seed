4fa834c62cc8e218270bd42e662a772dfece1370
Cay Horstmann <cay@horstmann.com>
2018-03-19 19:23:40 -0700

    Revert "Revert "Removed Hello""
    This reverts commit c9f93fac836d2b49fe9b6274a1317665d1482d11.
    
    It doesn't create the directories if you don't toss in the Hello files.

--- src/main/g8/src/test/scala/HelloSuite.scala
+++
@@ -1,7 +0,0 @@
-import org.scalatest.FunSuite
-
-class HelloSuite extends FunSuite {
-  test("Greeting CS152 students") {
-    assert(new Greeter("CS152 students").greet == "Hello, CS152 students!")
-  }
-}
